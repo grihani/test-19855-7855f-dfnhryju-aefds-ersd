730c41fe472026a154d63ec6f2a4f44395ef1589
grihani <grihani@csc.com>
2015-03-30 13:40:56 +0200

    Arrangé l'apparence en swrevealcontroller

---
+++ UIScrollViewForMobilite/DetailAccountsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailAccountsViewController.swift
+//  UIScrollViewForMobilite
+//
+//  Created by CSC CSC on 30/03/2015.
+//  Copyright (c) 2015 CSC CSC. All rights reserved.
+//
+
+import UIKit
+
+class DetailAccountsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showList(sender: UIBarButtonItem) {
+        self.revealViewController().revealToggle(sender)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UIScrollViewForMobilite/UIScrollViewForMobilite-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  UIScrollViewForMobilite-Bridging-Header.h
+//  UIScrollViewForMobilite
+//
+//  Created by CSC CSC on 30/03/2015.
+//  Copyright (c) 2015 CSC CSC. All rights reserved.
+//
+
+#ifndef UIScrollViewForMobilite_UIScrollViewForMobilite_Bridging_Header_h
+#define UIScrollViewForMobilite_UIScrollViewForMobilite_Bridging_Header_h
+
+
+#endif
+#import "SWRevealViewController.h"
