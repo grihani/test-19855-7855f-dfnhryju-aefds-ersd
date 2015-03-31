71ed0d26248c87f30aba6a96393bba6bcd5f0c74
grihani <grihani@csc.com>
2015-03-31 15:10:33 +0200

    added buttons in relation with the pages the scrollview gets to

--- UIScrollViewForMobilite/DetailAccountsViewController.swift
+++ UIScrollViewForMobilite/DetailAccountsViewController.swift
@@ -14,21 +14,24 @@ class DetailAccountsViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var showList: UIView!
     @IBOutlet weak var containerView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var listeButtonsForPages: UIView!
     
     //var identifiers: [String: AnyObject] = ["baba": NSClassFromString("Test1ViewController"), "boubou": NSClassFromString("Test2ViewController")]
     var identifiers: [String] = ["baba", "boubou", "bibi"]
     var firstPageC: Test1ViewController = Test1ViewController()
     var secondPage: Test2ViewController = Test2ViewController()
     var thirdPage: Test3ViewController = Test3ViewController()
-    var pageControllers: [AnyObject] = []
+    var pageControllers: [UIViewController] = []
     var pageViews: [UIView] = []
     var firstPage = 1
     
+    @IBOutlet var buttonPages: [UIButton] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.showList.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        buttonForPages(listeButtonsForPages)
+        
     }
     
     override func viewDidLayoutSubviews() {
@@ -45,6 +48,7 @@ class DetailAccountsViewController: UIViewController, UIScrollViewDelegate {
 
         let pageScrollViewSize = containerView.frame.size
         containerView.contentSize = CGSize(width: pageScrollViewSize.width * CGFloat(pageCount), height: pageScrollViewSize.height)
+        containerView.backgroundColor = UIColor(red: 0, green: 0, blue: 100, alpha: 0.2)
     
     }
 
@@ -52,8 +56,8 @@ class DetailAccountsViewController: UIViewController, UIScrollViewDelegate {
         super.didReceiveMemoryWarning()
     }
     
-    func getViewControllers() -> [AnyObject]{
-        var viewControllers: [AnyObject] = [AnyObject]()
+    func getViewControllers() -> [UIViewController]{
+        var viewControllers: [UIViewController] = [UIViewController]()
         if let viewController = self.storyboard?.instantiateViewControllerWithIdentifier(identifiers[0]) as? Test1ViewController {
             viewControllers.append(viewController)
             self.firstPageC = viewController
@@ -86,8 +90,16 @@ class DetailAccountsViewController: UIViewController, UIScrollViewDelegate {
         for i in 0..<numberOfPages {
             pageBoundsForViews.origin.x = CGFloat(i) * pageSize.width
             let newPageView = pageViews[i]
-            newPageView.frame = pageBoundsForViews.rectByInsetting(dx: 10, dy: 0)
+            newPageView.frame = pageBoundsForViews.rectByInsetting(dx: 1, dy: 1)
             containerView.addSubview(newPageView)
+        }
+    }
+    
+    @IBAction func goPage(sender: UIButton) {
+        let textLabel = sender.titleLabel?.text
+        var index = find(buttonPages, sender)
+        if let index = index {
+            showPage(index)
         }
     }
     
@@ -113,5 +125,22 @@ class DetailAccountsViewController: UIViewController, UIScrollViewDelegate {
     @IBAction func showList(sender: UIBarButtonItem) {
         self.revealViewController().revealToggle(sender)
         changeLabels()
+    }
+    
+    func buttonForPages(view: UIView) {
+        let frame = view.frame
+        var i = 0
+        let width = CGFloat(100)
+        for identifier in identifiers {
+            let button = UIButton(frame: CGRect(x: CGFloat(i) * width, y: 0, width: 50, height: 37))
+            button.setTitle(identifier, forState: UIControlState.Normal)
+            button.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+            
+            button.addTarget(self, action: Selector("goPage:"), forControlEvents: UIControlEvents.TouchUpInside)
+            view.addSubview(button)
+            buttonPages.append(button)
+            
+            i++
+        }
     }
 }
