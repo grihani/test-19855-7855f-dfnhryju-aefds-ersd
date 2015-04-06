f60125591ff4d6391fdb9740ac69e2ef07d5999c
Rihani Ghassane <rihani.ghassane@gmail.com>
2015-04-06 20:46:53 +0200

    saved Image of map

--- UIScrollViewForMobilite/Vue360ViewController.swift
+++ UIScrollViewForMobilite/Vue360ViewController.swift
@@ -7,39 +7,110 @@
 //
 
 import UIKit
+import MapKit
 
-class Vue360ViewController: UIViewController {
+class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UITextViewDelegate {
 
     @IBOutlet weak var nameCompany: UITextField!
-    @IBOutlet weak var labelTest: UILabel!
+    @IBOutlet weak var shortNameCompany: UITextField!
+    @IBOutlet weak var industryCompany: UITextField!
+    @IBOutlet weak var phoneCompany: UITextField!
+    @IBOutlet weak var mailCompany: UITextField!
+    @IBOutlet weak var webSite: UITextField!
+    @IBOutlet weak var adressCompany: UITextView!
+    @IBOutlet weak var mapAdressCompany: MKMapView!
+    
     var account: AccountModel!
+    var locationManager = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        println("chargement Vue 360")
+        
+        self.locationManager.delegate = self
+        self.locationManager.requestAlwaysAuthorization()
+        self.adressCompany.delegate = self
+        self.mapAdressCompany.delegate = self
+        self.locationManager.startUpdatingLocation()
+        
         if account != nil {
             self.nameCompany.text = account.nameAccount
+            self.shortNameCompany.text = account.shortNameAccount
+            self.industryCompany.text = account.industryAccount
+            self.phoneCompany.text = account.phoneAccount
+            self.webSite.text = account.websiteAccount
+            self.adressCompany.text = account.adressAccount
+            
+            geolocalise(address: account.adressAccount, account: account, mapView: self.mapAdressCompany)
         }
-        // Do any additional setup after loading the view.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func geolocalise (#address: String?, account: AccountModel, mapView: MKMapView){
+//        var address = address
+        if address != nil {
+            var geocoder = CLGeocoder()
+        
+            geocoder.geocodeAddressString(address, completionHandler: {(placemarks: [AnyObject]!, error: NSError!) -> Void in
+                if let placemark = placemarks?[0] as? CLPlacemark {
+                    let span = MKCoordinateSpanMake(0.01, 0.01)
+                    let region = MKCoordinateRegionMake(placemark.location.coordinate, span)
+                    mapView.setRegion(region, animated: false)
+                    let annotation = MKPointAnnotation()
+                    print("les coordonnées sont : ")
+                    print(placemark.location.coordinate.latitude)
+                    print(", ")
+                    println(placemark.location.coordinate.longitude)
+                    annotation.setCoordinate(placemark.location.coordinate)
+                    annotation.title = account.nameAccount
+                    annotation.subtitle = account.phoneAccount
+                    mapView.addAnnotation(annotation)
+                    mapView.selectAnnotation(annotation, animated: true)
+                    
+                    var options = MKMapSnapshotOptions()
+                    options.region = mapView.region
+                    options.size = mapView.frame.size
+                    options.scale = UIScreen.mainScreen().scale
+                    let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+                    let docsDir = dirPaths[0] as String
+                    var error: NSError?
+                    var filePath = docsDir.stringByAppendingPathComponent("Maps/\(account.idAccount)/")
+                    if !NSFileManager.defaultManager().fileExistsAtPath(filePath){
+                        NSFileManager.defaultManager().createDirectoryAtPath(filePath, withIntermediateDirectories: true, attributes: nil, error: &error)
+                    }
+                    filePath = filePath.stringByAppendingPathComponent("\(account.adressAccount).png")
+                    var snapshotter = MKMapSnapshotter(options: options)
+                    snapshotter.startWithCompletionHandler({ (snapshot, error) -> Void in
+                        if ((error) != nil) {
+                            NSLog("[Error] %@", error);
+                            return;
+                        }
+                        println("I'm trying to save the image here")
+                        let image = snapshot.image
+                        let data: NSData = UIImagePNGRepresentation(image)
+                        if data.writeToFile(filePath, atomically: true){
+                            println("l'enregistrement s'est bien passé")
+                            println(filePath)
+                            
+                        }
+                    })
+                }
+            })
+            
+        }
     }
-    */
-
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        println(locations.last)
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        geolocalise(address: textView.text, account: self.account, mapView: self.mapAdressCompany)
+    }
     @IBAction func finishedEditCompany(sender: UITapGestureRecognizer) {
         self.view.endEditing(true)
     }
