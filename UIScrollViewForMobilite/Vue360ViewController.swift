34bc744ebde6b3079da7c83d241075fa964a5f73
grihani <grihani@csc.com>
2015-04-07 10:47:19 +0200

    Enregistrement d'images se fait pour la map il manque absolument que je refasse tout le process de relecture des enregistrements

--- UIScrollViewForMobilite/Vue360ViewController.swift
+++ UIScrollViewForMobilite/Vue360ViewController.swift
@@ -19,10 +19,12 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
     @IBOutlet weak var webSite: UITextField!
     @IBOutlet weak var adressCompany: UITextView!
     @IBOutlet weak var mapAdressCompany: MKMapView!
+    @IBOutlet weak var leftView: UIView!
     
     var account: AccountModel!
     var locationManager = CLLocationManager()
-
+    let fileManager = NSFileManager.defaultManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,7 +33,9 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
         self.adressCompany.delegate = self
         self.mapAdressCompany.delegate = self
         self.locationManager.startUpdatingLocation()
-        
+    }
+    
+    override func viewDidLayoutSubviews() {
         if account != nil {
             self.nameCompany.text = account.nameAccount
             self.shortNameCompany.text = account.shortNameAccount
@@ -39,19 +43,86 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
             self.phoneCompany.text = account.phoneAccount
             self.webSite.text = account.websiteAccount
             self.adressCompany.text = account.adressAccount
-            
-            geolocalise(address: account.adressAccount, account: account, mapView: self.mapAdressCompany)
         }
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func geolocaliseAvecImage(#address: String?, account: AccountModel, mapView: MKMapView) {
+        if let address = address {
+            // if the image exists put the image in place of the mapView
+            // else create the mapView then show the image in its place
+            if checkImageExist(address: address, account: account) {
+                var frame = mapView.frame
+                mapView.removeFromSuperview()
+                var pathImage = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+                pathImage = pathImage.stringByAppendingPathComponent("Maps/\(account.idAccount)/\(account.adressAccount).png")
+                var image = UIImageView(image: UIImage(contentsOfFile: pathImage))
+                image.frame = frame
+                self.leftView.addSubview(image)
+            } else {
+                var geocoder = CLGeocoder()
+                geocoder.geocodeAddressString(address, completionHandler: {(placemarks: [AnyObject]!, error: NSError!) -> Void in
+                    println(placemarks)
+                    if let placemark = placemarks?[0] as? CLPlacemark {
+                        if placemark.location != nil {
+                            let span = MKCoordinateSpanMake(0.01, 0.01)
+                            let region = MKCoordinateRegionMake(placemark.location.coordinate, span)
+                            mapView.setRegion(region, animated: false)
+                            let annotation = MKPointAnnotation()
+                            print("les coordonnées sont : ")
+                            print(placemark.location.coordinate.latitude)
+                            print(", ")
+                            println(placemark.location.coordinate.longitude)
+                            annotation.setCoordinate(placemark.location.coordinate)
+                            annotation.title = account.nameAccount
+                            annotation.subtitle = account.phoneAccount
+                            mapView.addAnnotation(annotation)
+                            mapView.selectAnnotation(annotation, animated: true)
+                        } else {
+                            println("the adress is not correct")
+                            mapView.removeFromSuperview()
+                        }
+                    }
+                })
+            }
+        }
+    }
+    
+    // check if there's an image in the repertory for the mapview
+    func checkImageExist(#address: String, account: AccountModel) -> Bool{
+        var error: NSError?
+        var paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        var dirPath = paths.stringByAppendingPathComponent("Maps/\(account.idAccount)/")
+        if !fileManager.fileExistsAtPath(dirPath){
+            fileManager.createDirectoryAtPath(dirPath, withIntermediateDirectories: true, attributes: nil, error: &error)
+            return false
+        } else {
+            var filePath = dirPath.stringByAppendingPathComponent("\(account.adressAccount).png")
+            if !fileManager.fileExistsAtPath(filePath){
+                // deleting all files in the directory
+                var i = 0
+                var directoryFiles = fileManager.contentsOfDirectoryAtPath(dirPath, error: &error)
+                if let directoryFiles = directoryFiles {
+                    for file in directoryFiles {
+                        i++
+                        print("suppression \(i): ")
+                        let success = fileManager.removeItemAtPath("\(dirPath)/\(file)", error: &error)
+                        if (!success || error != nil) {
+                            println(error)
+                        }
+                    }
+                }
+                return false
+            }
+        }
+        return true
+    }
+    
+    // that's just to geolocalise an adress without creating an image
     func geolocalise (#address: String?, account: AccountModel, mapView: MKMapView){
-//        var address = address
         if address != nil {
             var geocoder = CLGeocoder()
         
@@ -70,34 +141,6 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
                     annotation.subtitle = account.phoneAccount
                     mapView.addAnnotation(annotation)
                     mapView.selectAnnotation(annotation, animated: true)
-                    
-                    var options = MKMapSnapshotOptions()
-                    options.region = mapView.region
-                    options.size = mapView.frame.size
-                    options.scale = UIScreen.mainScreen().scale
-                    let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
-                    let docsDir = dirPaths[0] as String
-                    var error: NSError?
-                    var filePath = docsDir.stringByAppendingPathComponent("Maps/\(account.idAccount)/")
-                    if !NSFileManager.defaultManager().fileExistsAtPath(filePath){
-                        NSFileManager.defaultManager().createDirectoryAtPath(filePath, withIntermediateDirectories: true, attributes: nil, error: &error)
-                    }
-                    filePath = filePath.stringByAppendingPathComponent("\(account.adressAccount).png")
-                    var snapshotter = MKMapSnapshotter(options: options)
-                    snapshotter.startWithCompletionHandler({ (snapshot, error) -> Void in
-                        if ((error) != nil) {
-                            NSLog("[Error] %@", error);
-                            return;
-                        }
-                        println("I'm trying to save the image here")
-                        let image = snapshot.image
-                        let data: NSData = UIImagePNGRepresentation(image)
-                        if data.writeToFile(filePath, atomically: true){
-                            println("l'enregistrement s'est bien passé")
-                            println(filePath)
-                            
-                        }
-                    })
                 }
             })
             
@@ -108,9 +151,45 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
         println(locations.last)
     }
     
+    // rendering a picture for the mapview
+    func pictureForView(view: UIView) -> UIImage {
+        UIGraphicsBeginImageContext(view.frame.size)
+        var ctx: CGContextRef = UIGraphicsGetCurrentContext()
+        view.layer.renderInContext(ctx)
+        var image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
+    func mapViewDidFinishRenderingMap(mapView: MKMapView!, fullyRendered: Bool) {
+        if account != nil {
+            
+            
+            geolocaliseAvecImage(address: account.adressAccount, account: account, mapView: self.mapAdressCompany)
+            var frame = mapView.frame
+            var mapImage: UIImage = self.pictureForView(mapView)
+            var paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+            var dirPath = paths.stringByAppendingPathComponent("Maps/\(account.idAccount)/")
+            var imagePath = dirPath.stringByAppendingPathComponent("\(account.adressAccount).png")
+            if !fileManager.fileExistsAtPath(imagePath) {
+                fileManager.createDirectoryAtPath(dirPath, withIntermediateDirectories: true, attributes: nil, error: nil)
+                UIImagePNGRepresentation(mapImage).writeToFile(imagePath, atomically: true)
+                
+            }
+            mapView.removeFromSuperview()
+            var image = UIImageView(image: UIImage(contentsOfFile: imagePath))
+            image.frame = frame
+            self.leftView.addSubview(image)
+            
+            println(imagePath)
+            println("finished rendering map")
+        }
+    }
+    
     func textViewDidEndEditing(textView: UITextView) {
         geolocalise(address: textView.text, account: self.account, mapView: self.mapAdressCompany)
     }
+    
     @IBAction func finishedEditCompany(sender: UITapGestureRecognizer) {
         self.view.endEditing(true)
     }
