e447330ac0edd7a5eff638688bb9889e8e240ea3
grihani <grihani@csc.com>
2015-04-07 11:32:04 +0200

    enregistrement de photos a la place des maps leur affichage

--- UIScrollViewForMobilite/Vue360ViewController.swift
+++ UIScrollViewForMobilite/Vue360ViewController.swift
@@ -24,6 +24,7 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
     var account: AccountModel!
     var locationManager = CLLocationManager()
     let fileManager = NSFileManager.defaultManager()
+    var imageExists = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,9 +34,6 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
         self.adressCompany.delegate = self
         self.mapAdressCompany.delegate = self
         self.locationManager.startUpdatingLocation()
-    }
-    
-    override func viewDidLayoutSubviews() {
         if account != nil {
             self.nameCompany.text = account.nameAccount
             self.shortNameCompany.text = account.shortNameAccount
@@ -45,7 +43,7 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
             self.adressCompany.text = account.adressAccount
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -56,7 +54,9 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
             // else create the mapView then show the image in its place
             if checkImageExist(address: address, account: account) {
                 var frame = mapView.frame
+                self.imageExists = true
                 mapView.removeFromSuperview()
+                self.mapAdressCompany = nil
                 var pathImage = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
                 pathImage = pathImage.stringByAppendingPathComponent("Maps/\(account.idAccount)/\(account.adressAccount).png")
                 var image = UIImageView(image: UIImage(contentsOfFile: pathImage))
@@ -65,9 +65,9 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
             } else {
                 var geocoder = CLGeocoder()
                 geocoder.geocodeAddressString(address, completionHandler: {(placemarks: [AnyObject]!, error: NSError!) -> Void in
-                    println(placemarks)
                     if let placemark = placemarks?[0] as? CLPlacemark {
                         if placemark.location != nil {
+                            self.imageExists = false
                             let span = MKCoordinateSpanMake(0.01, 0.01)
                             let region = MKCoordinateRegionMake(placemark.location.coordinate, span)
                             mapView.setRegion(region, animated: false)
@@ -78,7 +78,6 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
                             println(placemark.location.coordinate.longitude)
                             annotation.setCoordinate(placemark.location.coordinate)
                             annotation.title = account.nameAccount
-                            annotation.subtitle = account.phoneAccount
                             mapView.addAnnotation(annotation)
                             mapView.selectAnnotation(annotation, animated: true)
                         } else {
@@ -161,18 +160,22 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
         return image
     }
     
-    func mapViewDidFinishRenderingMap(mapView: MKMapView!, fullyRendered: Bool) {
+    // it will check if there's an image here so that we put it up
+    func mapViewWillStartRenderingMap(mapView: MKMapView!) {
         if account != nil {
-            
-            
             geolocaliseAvecImage(address: account.adressAccount, account: account, mapView: self.mapAdressCompany)
+        }
+    }
+    
+    func mapViewDidFinishRenderingMap(mapView: MKMapView!, fullyRendered: Bool) {
+        if account != nil && !self.imageExists {
             var frame = mapView.frame
             var mapImage: UIImage = self.pictureForView(mapView)
             var paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
             var dirPath = paths.stringByAppendingPathComponent("Maps/\(account.idAccount)/")
             var imagePath = dirPath.stringByAppendingPathComponent("\(account.adressAccount).png")
             if !fileManager.fileExistsAtPath(imagePath) {
-                fileManager.createDirectoryAtPath(dirPath, withIntermediateDirectories: true, attributes: nil, error: nil)
+//                fileManager.createDirectoryAtPath(dirPath, withIntermediateDirectories: true, attributes: nil, error: nil)
                 UIImagePNGRepresentation(mapImage).writeToFile(imagePath, atomically: true)
                 
             }
@@ -182,8 +185,8 @@ class Vue360ViewController: UIViewController, MKMapViewDelegate, CLLocationManag
             self.leftView.addSubview(image)
             
             println(imagePath)
-            println("finished rendering map")
         }
+        println("finished rendering map")
     }
     
     func textViewDidEndEditing(textView: UITextView) {
